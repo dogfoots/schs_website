5eaa8cf51da0c79ae361a73acfc7f23eaaa88165
Hee-Cheol Kim <41623476+dogfoots@users.noreply.github.com>
2022-05-12 16:21:12 +0900

    1
    1

--- src/main/java/edu/schs/website/controller/MainController.java
+++ src/main/java/edu/schs/website/controller/MainController.java
@@ -1,13 +1,14 @@
 package edu.schs.website.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 	
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World!";
+		return "index";
 	}
 }
