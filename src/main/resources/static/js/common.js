f3e1fc243b826732c66e3468000a509c5d744107
Hee-Cheol Kim <41623476+dogfoots@users.noreply.github.com>
2022-07-27 14:53:54 +0900

    1
    1

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -110,3 +110,69 @@ function notAvailableService(){
 function goTop(){
     $('html, body').animate({ scrollTop: 0 }, 'fast');
 }
+
+
+// 쿠키 가져오기  
+function getCookie(name) {
+    var nameOfCookie = name + "=";
+    var x = 0;
+    while (x <= document.cookie.length) {
+      var y = (x + nameOfCookie.length);
+      if (document.cookie.substring(x, y) == nameOfCookie) {
+        if ((endOfCookie = document.cookie.indexOf(";", y)) == -1)
+          endOfCookie = document.cookie.length;
+        return unescape(document.cookie.substring(y, endOfCookie));
+      }
+      x = document.cookie.indexOf(" ", x) + 1;
+      if (x == 0)
+        break;
+    }
+    return "";
+  }
+
+  // 24시간 기준 쿠키 설정하기  
+  // 만료 후 클릭한 시간까지 쿠키 설정  
+  function setCookie(name, value, expiredays) {
+    var todayDate = new Date();
+    todayDate.setDate(todayDate.getDate() + expiredays);
+    document.cookie = name + "=" + escape(value) + "; path=/; expires=" + todayDate.toGMTString() + ";"
+  }
+  
+  // 창열기  
+  function todayPopOpen(winName) {
+    var blnCookie = getCookie(winName);
+    //var obj = eval("window." + winName);
+    //console.log(blnCookie);
+    if (!blnCookie) {
+        $("#"+winName).show();
+      //obj.style.display = "block";
+    } else {
+        $("#"+winName).hide();
+      //obj.style.display = "none";
+    }
+  }
+  // 창닫기  
+  function todayPopClose(winName, expiredays) {
+    setCookie(winName, "expire", expiredays);
+    $("#"+winName).hide();
+    //var obj = eval("window." + winName);
+    //obj.style.display = "none";
+  }
+
+  function popClose(winName){
+    //var obj = eval("window." + winName);
+    $("#"+winName).hide();
+    //obj.style.display = "none";
+  }
+
+  $(document).ready(function(){
+        $("#popup-notice-wrap").on("click",function(){
+            popClose('popup-notice-wrap');
+        });
+
+        $(".popup-notice").on("click",function(){
+
+        });
+        
+        todayPopOpen('popup-notice-wrap');
+  });
